826f125ef577d2c186dc49eb43307202a96ec3b2
direct66612 <direct66612@gmail.com>
2023-10-05 16:41:11 -0400

    update

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import Notiflix from 'notiflix';
 
 const select = document.querySelector('.breed-select');
 const catInfo = document.querySelector('.cat-info');
-const loader = document.querySelector('.loader');
-
+const loader = document.querySelector('.wrapper');
+loader.classList.remove('is-hidden');
 fetchBreeds()
   .then(data => {
     data.map(elem => {
@@ -17,12 +17,27 @@ fetchBreeds()
     });
   })
   .catch(error => {
-    Notiflix.Notify.failure('Qui timide rogat docet negare');
+    Notiflix.Notify.failure(
+      'Oops! Something went wrong! Try reloading the page!'
+    );
+  })
+  .finally(() => {
+    loader.classList.add('is-hidden');
   });
 function handleClick(event) {
-  fetchCatByBreed(event.target.value).then(([data]) => {
-    const { url, breeds } = data;
-    const { name, description, temperament } = breeds[0];
-    catInfo.innerHTML = `<img src="${url}" alt="${name}"/><h1>${name}</h1><p>${description}</p><p><b>Temperament:</b>${temperament}</p>`;
-  });
+  loader.classList.remove('is-hidden');
+  fetchCatByBreed(event.target.value)
+    .then(([data]) => {
+      const { url, breeds } = data;
+      const { name, description, temperament } = breeds[0];
+      catInfo.innerHTML = `<img src="${url}" alt="${name}"/><h1>${name}</h1><p>${description}</p><p><b>Temperament:</b>${temperament}</p>`;
+    })
+    .catch(error => {
+      Notiflix.Notify.failure(
+        'Oops! Something went wrong! Try reloading the page!'
+      );
+    })
+    .finally(() => {
+      loader.classList.add('is-hidden');
+    });
 }
